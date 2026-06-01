f2bdd1d2eb1ec880c23fc33e22a28264fc3b9eb6
Surya Rastogi <suryarastogi01@gmail.com>
2026-06-01 14:59:15 +0530

    Week 1 Day 1 completed

--- Week_1_Arrays_Strings/Day_1_Array_Basics/Best_Time_To_Buy_And_Sell_Stock.cpp
+++ Week_1_Arrays_Strings/Day_1_Array_Basics/Best_Time_To_Buy_And_Sell_Stock.cpp
@@ -1,1 +1,23 @@
 // Best_Time_To_Buy_And_Sell_Stock
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+                                  //INT_MAX is the maximum value an integer can hold.
+        int minPrice = INT_MAX;   // Minimum price seen so far
+        int maxProfit = 0;        // Maximum profit
+        
+        for(int i = 0; i < prices.size(); i++) {
+            
+            // Update minimum buying price
+            minPrice = min(minPrice, prices[i]);
+            
+            // Calculate profit if sold today
+            int profit = prices[i] - minPrice;
+            
+            // Update maximum profit
+            maxProfit = max(maxProfit, profit);
+        }
+        
+        return maxProfit;
+    }
+};
--- Week_1_Arrays_Strings/Day_1_Array_Basics/Remove_Duplicates_From_Sorted_Array.cpp
+++ Week_1_Arrays_Strings/Day_1_Array_Basics/Remove_Duplicates_From_Sorted_Array.cpp
@@ -1,1 +1,14 @@
 // Remove_Duplicates_From_Sorted_Array
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int ans = 1;
+        for(int i=1; i<nums.size(); ++i) {
+            if(nums[i] != nums[i-1]) {
+                nums[ans] = nums[i];
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
