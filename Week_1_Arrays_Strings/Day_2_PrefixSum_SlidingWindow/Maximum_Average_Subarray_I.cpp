8e8da9e563eac289fb50dbc748cea2b7ef354a43
Surya Rastogi <suryarastogi01@gmail.com>
2026-06-02 13:28:27 +0530

    Week 1 Day 2 completed

--- Week_1_Arrays_Strings/Day_2_PrefixSum_SlidingWindow/Maximum_Average_Subarray_I.cpp
+++ Week_1_Arrays_Strings/Day_2_PrefixSum_SlidingWindow/Maximum_Average_Subarray_I.cpp
@@ -1,1 +1,25 @@
 // Maximum_Average_Subarray_I
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+
+        long long sum = 0;
+
+        // First window sum
+        for(int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        long long maxSum = sum;     //Initialize maximum sum.
+
+        // Slide the window
+        for(int i = k; i < nums.size(); i++) {   //Because the first k elements are already used to calculate the first window sum.
+
+            sum = sum - nums[i - k] + nums[i];  //new sum = old sum - leaving element + entering element
+
+            maxSum = max(maxSum, sum);
+        }
+
+        return (double)maxSum / k;
+    }
+};
--- Week_1_Arrays_Strings/Day_2_PrefixSum_SlidingWindow/Maximum_Subarray.cpp
+++ Week_1_Arrays_Strings/Day_2_PrefixSum_SlidingWindow/Maximum_Subarray.cpp
@@ -1,1 +1,26 @@
 // Maximum_Subarray
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int maxSum = nums[0];   // Stores the final maximum subarray sum
+        int currSum = 0;        // Stores current running sum
+        
+        for(int i = 0; i < nums.size(); i++) {
+            
+            // Add current element to running sum
+            currSum += nums[i];
+            
+            // Update maximum sum if current sum is greater
+            maxSum = max(maxSum, currSum);
+            
+            // If running sum becomes negative,
+            // reset it to 0 because it will not help in future
+            if(currSum < 0) {
+                currSum = 0;
+            }
+        }
+        
+        return maxSum;
+    }
+};
