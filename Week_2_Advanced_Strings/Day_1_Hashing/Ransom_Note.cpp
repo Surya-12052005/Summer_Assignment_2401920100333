693de3c332237668aad6bfbf9136ad2fb6e43a19
Surya Rastogi <suryarastogi01@gmail.com>
2026-06-08 13:07:18 +0530

    Week 2 Day 1 completed

--- Week_2_Advanced_Strings/Day_1_Hashing/Ransom_Note.cpp
+++ Week_2_Advanced_Strings/Day_1_Hashing/Ransom_Note.cpp
@@ -1,1 +1,20 @@
 // Ransom_Note
+class Solution {
+public:
+    bool canConstruct(string ran, string mag) {
+        sort(ran.begin(), ran.end());
+        sort(mag.begin(), mag.end());
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        while(j < mag.size()) {
+            if(ran[i] == mag[j]) {
+                count++;
+                i++;
+                j++;
+            }
+            else j++;
+        }
+        return count == ran.size();  //If all ransom-note characters were matched, return true.
+    }
+};
--- Week_2_Advanced_Strings/Day_1_Hashing/Valid_Anagram.cpp
+++ Week_2_Advanced_Strings/Day_1_Hashing/Valid_Anagram.cpp
@@ -1,1 +1,12 @@
 // Valid_Anagram
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size()!=t.size()) return false;
+        
+        sort(s.begin(), s.end());     //"cab" → "abc"
+        sort(t.begin(), t.end());
+        
+        return s==t;            //If equal → true
+    }
+};
