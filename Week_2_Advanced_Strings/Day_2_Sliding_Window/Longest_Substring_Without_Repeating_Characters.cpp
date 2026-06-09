b010e37fa850e99bb813bf2c4c1a5e95f08819ca
Surya Rastogi <suryarastogi01@gmail.com>
2026-06-09 23:22:16 +0530

    Week 2 Day 2 completed

--- Week_2_Advanced_Strings/Day_2_Sliding_Window/Longest_Substring_Without_Repeating_Characters.cpp
+++ Week_2_Advanced_Strings/Day_2_Sliding_Window/Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,1 +1,24 @@
 // Longest_Substring_Without_Repeating_Characters
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {// unordered_set-Helps check duplicates in O(1) time
+        unordered_set<char> st;  // variable st-that stores unique characters
+        int left = 0, maxLen = 0;  //  unordered_set-A data structure from C++ STL
+
+        for (int right = 0; right < s.length(); right++) {
+            // If duplicate found, remove from left
+            while (st.find(s[right]) != st.end()) {  //s[right]- this is the current character.
+                st.erase(s[left]);                  //st.find(s[right])-Checks if this character is already present in the set
+                left++;                            //st.end() → if NOT found
+            }                                     //!= st.end()-Means: character is found in the set
+
+            // Add current character
+            st.insert(s[right]);  //Add the current character into the set
+
+            // Update max length
+            maxLen = max(maxLen, right - left + 1);//Calculate current window size &Update maximum length
+        }
+
+        return maxLen;
+    }
+};
