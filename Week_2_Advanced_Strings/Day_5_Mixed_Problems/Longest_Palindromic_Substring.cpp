08f1194e0fefe243180e1c840f7d6ab485631ca3
Surya Rastogi <suryarastogi01@gmail.com>
2026-06-12 16:02:51 +0530

    Week X Day Y completed

--- Week_2_Advanced_Strings/Day_5_Mixed_Problems/Longest_Palindromic_Substring.cpp
+++ Week_2_Advanced_Strings/Day_5_Mixed_Problems/Longest_Palindromic_Substring.cpp
@@ -1,1 +1,37 @@
 // Longest_Palindromic_Substring
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        string res = "";
+
+        for(int i = 0; i < n; i++) {
+
+            // 🔹 1. Odd length palindrome (center = i)
+            int left = i, right = i;
+
+            while(left >= 0 && right < n && s[left] == s[right]) {
+                // Update result if longer palindrome found
+                if(right - left + 1 > res.size()) {    //gives length of current substring
+                    res = s.substr(left, right - left + 1);  //Extracts substring from index left of given length
+                }
+                left--;
+                right++;
+            }
+
+            // 🔹 2. Even length palindrome (center = i, i+1)
+            left = i;
+            right = i + 1;
+
+            while(left >= 0 && right < n && s[left] == s[right]) {
+                if(right - left + 1 > res.size()) {
+                    res = s.substr(left, right - left + 1);
+                }
+                left--;
+                right++;
+            }
+        }
+
+        return res;
+    }
+};
--- Week_2_Advanced_Strings/Day_5_Mixed_Problems/String_Compression.cpp
+++ Week_2_Advanced_Strings/Day_5_Mixed_Problems/String_Compression.cpp
@@ -1,1 +1,30 @@
 // String_Compression
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int write = 0, count = 1;
+        int n = chars.size();
+        
+        for (int read = 0; read < n; ++read) {
+            // Check if we reached the end of a group of repeating characters
+            if (read + 1 == n || chars[read] != chars[read + 1]) {
+                // 1. Write the character itself
+                chars[write++] = chars[read];
+                
+                // 2. If the character repeated, write its frequency digits
+                if (count > 1) {
+                    for (char digit : to_string(count)) {
+                        chars[write++] = digit;
+                    }
+                }
+                // Reset count for the next group
+                count = 1; 
+            } else {
+                // Same character continues, increment count
+                count++; 
+            }
+        }
+        // The total length of the compressed string
+        return write;
+    }
+};
